170104dab54a0e88489d8f67f2287fd14e215fa0
Renevc14 <rene.vilela@ucb.edu.bo>
2026-05-11 22:09:39 -0400

    feat(infra): agrega ApiGatewayStack con JWT Authorizer y endpoint /v1/me

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
+import { ApiGatewayStack } from '../lib/stacks/api-gateway-stack';
 import { AuthentikStack } from '../lib/stacks/authentik-stack';
 import { NetworkStack } from '../lib/stacks/network-stack';
 import { SecretsStack } from '../lib/stacks/secrets-stack';
@@ -14,8 +15,13 @@ const env = {
 const network = new NetworkStack(app, 'NetworkStack', { env });
 const secrets = new SecretsStack(app, 'SecretsStack', { env });
 
-new AuthentikStack(app, 'AuthentikStack', {
+const authentik = new AuthentikStack(app, 'AuthentikStack', {
   env,
   vpc: network.vpc,
   authentikSecretKey: secrets.authentikSecretKey,
+});
+
+new ApiGatewayStack(app, 'ApiGatewayStack', {
+  env,
+  authentikPublicIp: authentik.publicIp,
 });
