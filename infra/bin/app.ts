8b9c3c34eb227f344112bcf8c41e8114f040fb36
Renevc14 <rene.vilela@ucb.edu.bo>
2026-05-11 21:57:12 -0400

    refactor(infra): elimina DatabaseStack (Postgres ahora corre dentro del EC2 de Authentik)

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
-import { DatabaseStack } from '../lib/stacks/database-stack';
 import { NetworkStack } from '../lib/stacks/network-stack';
 import { SecretsStack } from '../lib/stacks/secrets-stack';
 
@@ -11,6 +10,5 @@ const env = {
   region: process.env.CDK_DEFAULT_REGION,
 };
 
-const network = new NetworkStack(app, 'NetworkStack', { env });
+new NetworkStack(app, 'NetworkStack', { env });
 new SecretsStack(app, 'SecretsStack', { env });
-new DatabaseStack(app, 'DatabaseStack', { env, vpc: network.vpc });
